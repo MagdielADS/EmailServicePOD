34013ce3941dea0fb7e60650041e99837197df30
fernandodof <fernandodof@gmail.com>
2014-09-03 15:40:48 -0300

    Atualizando

--- src/main/java/persistence/EmailDAO.java
+++ src/main/java/persistence/EmailDAO.java
@@ -3,13 +3,97 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package persistence;
+
+import br.edu.ifpb.emailsharedpod.Email;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author magdiel-bruno
  */
 public class EmailDAO {
+
+    public boolean save(Email email) {
+        String sql = "INERT INTO EMAIL(remetente,destinatarios,assunto,status,mensagem) VALUES(?,?,?,?,?)";
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            try {
+                stmt.setString(1, email.getRemetente());
+                stmt.setString(2, email.getDestinatarios());
+                stmt.setString(3, email.getAssunto());
+                stmt.setBoolean(4, email.isStatus());
+                stmt.setString(5, email.getMensagem());
+                stmt.executeQuery();
+                return true;
+            } finally {
+                stmt.close();
+                connection.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EmailDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean updateStatusToSent(int id) {
+        String sql = "UPDATE EMAIL SET status=true WHERE id = ?";
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            try {
+                stmt.setInt(1, id);
+                stmt.executeUpdate();
+                return true;
+            } finally {
+                stmt.close();
+                connection.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EmailDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public List<Email> getUnsentEmails() {
+        String sql = "SELECT id,remetente,destinatarios,assunto,status,mensagem FROM email WHERE status = false";
+        List<Email> emails = new ArrayList<Email>();
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            try {
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()) {
+                    Email email = new Email();
+                    email.setId(rs.getInt("id"));
+                    email.setMensagem(rs.getString("mensagem"));
+                    email.setRemetente(rs.getString("remetente"));
+                    email.setDestinatarios(rs.getString("destinatarios"));
+                    email.setAssunto(rs.getString("assunto"));
+                    email.setStatus(rs.getBoolean("status"));
+                    emails.add(email);
+                }
+                return emails;
+            } finally {
+                stmt.close();
+                connection.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EmailDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
     
 }
