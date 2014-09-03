43982a78fbcc0e9eba64bdbdcaf723e5f86a812b
magdiel.ildefonso <magdiel.ildefonso@gmail.com>
2014-09-03 17:01:00 -0300

    Iniciando pings

--- src/main/java/test/AppMagdielBruno.java
+++ src/main/java/test/AppMagdielBruno.java
@@ -6,12 +6,26 @@
 
 package test;
 
+import br.edu.ifpb.emailsharedpod.Email;
+import tolerance.FaultTolerance;
+import util.EmailUtil;
+
 /**
  *
  * @author magdiel-bruno
  */
 public class AppMagdielBruno {
     public static void main(String[] args) {
+        Email email = new Email();
         
+        email.setAssunto("Porra nenhuma");
+        email.setMensagem("Ta saindo do lugar");
+        email.setRemetente("Sou eu!");
+        email.setDestinatarios("magdiel.ildefonso@gmail.com, fernandodof@gmail.com, kelsonsd@gmail.com");
+        
+        //EmailUtil.sendEmail(email);
+        
+        //System.out.println(FaultTolerance.ping());
+        //System.out.println(FaultTolerance.pingServer("10.1.1.120"));
     }
 }
