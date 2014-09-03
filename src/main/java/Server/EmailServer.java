ba34ae2f31710a05a1ef8055ad238bd65273b10d
magdiel.ildefonso <magdiel.ildefonso@gmail.com>
2014-09-03 15:44:58 -0300

    Enviando email

--- src/main/java/Server/EmailServer.java
+++ src/main/java/Server/EmailServer.java
@@ -11,6 +11,15 @@ import br.edu.ifpb.emailsharedpod.Fachada;
 import br.edu.ifpb.emailsharedpod.Pessoa;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  *
@@ -20,7 +29,39 @@ public class EmailServer implements Fachada{
 
     @Override
     public String enviaEmail(Email email) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String result = "Email não enviado, aguarde tentaremos reestabelecer a conexão";
+        
+        final String from = "ifpbpod@gmail.com";
+        final String pass = "rmi12345";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, pass);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(email.getRemetente()));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(email.getDestinatarios()));
+            message.setSubject(email.getAssunto());
+            message.setText(email.getMensagem());
+
+            Transport.send(message);
+            
+            result = "Email enviado com sucesso";
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
@@ -32,5 +73,4 @@ public class EmailServer implements Fachada{
     public List<Pessoa> listaPessoas() throws RemoteException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 }
